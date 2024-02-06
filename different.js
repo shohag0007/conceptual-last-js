122b227303cf0390fe68c0b3efbe8b91fb931deb
shohag0007 <merinedrive007@gmail.com>
2024-02-06 18:20:09 +0600

    print name

--- different.js
+++ different.js
@@ -10,3 +10,42 @@ console.log(`My name is ${name}. My age is ${age}. My address is ${address}`);
 }
 
 printDetails("Md Delowar Hossain Shohag", "35 Years", "Dulabala, kaligonj, Satkhira");
+
+//
+
+function add(x,y){
+    const total = x + y;
+    return total;
+}
+console.log(add(50,50));
+// const addition = add(50, 50);
+// console.log(addition);
+
+
+// 
+
+function evenOdd(x){
+    
+    if(x % 2 === 0){
+    return "Even";
+    }
+
+    else{
+     return "Odd";
+    }
+ }
+
+
+// const result = evenOdd(9);
+// console.log(result) 
+
+ function alifPrint(x){
+    for(let i = 1; i <= x; i++){
+        console.log("shohag");
+    }
+ }
+
+//  const result = 10;
+//  alifPrint(result);
+
+alifPrint(10);
