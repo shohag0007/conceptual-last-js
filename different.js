a81399e2c02014e10a842a5e13e1a738ea2d16e9
shohag0007 <merinedrive007@gmail.com>
2024-02-06 13:23:01 +0600

    details address

---
+++ different.js
@@ -0,0 +1,12 @@
+// function printName(name){
+//     console.log('my name is', name);
+// }
+
+// printName("shohag");
+
+function printDetails(name,age,address){
+console.log(`My name is ${name}. My age is ${age}. My address is ${address}`);
+
+}
+
+printDetails("Md Delowar Hossain Shohag", "35 Years", "Dulabala, kaligonj, Satkhira");
